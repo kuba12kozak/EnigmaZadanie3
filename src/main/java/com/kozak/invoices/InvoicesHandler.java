00ed06b573b4c29cb2ec02940adf6a7e0d4cf20d
jakubkozak <kuba12kozak@gmail.com>
2021-11-07 20:30:51 +0100

    Added API docs

--- src/main/java/com/kozak/invoices/InvoicesHandler.java
+++ src/main/java/com/kozak/invoices/InvoicesHandler.java
@@ -8,7 +8,30 @@ import java.util.Map;
 
 public interface InvoicesHandler {
 
+    /**
+     * Creates an invoice in Stripe system and returns created invoice item
+     *
+     * @param apiKey Stripe's apiKey
+     * @param customer customer id in Stripe system
+     * @param invoiceAdditionalDetails map containing additional invoice details
+     * @param currency invoice currency
+     * @param amount amount to be paid
+     * @param invoiceItemAdditionalDetails map containing additional invoice items details
+     * @throws StripeAPIException for any exception thrown by Stripe service
+     * @throws ValidationException for any validation exception
+     * @return invoice item
+     */
     Invoice createInvoice(String apiKey, String customer, Map<String, String> invoiceAdditionalDetails, String currency, Integer amount, Map<String, String> invoiceItemAdditionalDetails) throws StripeAPIException, ValidationException;
 
+
+    /**
+     * Returns an Invoice item for a given id.
+     *
+     * @param apiKey Stripe's apiKey
+     * @param invoiceId id of the invoice to be retrieved from Stripe
+     * @throws StripeAPIException for any exception thrown by Stripe service
+     * @throws ValidationException for any validation exception
+     * @return invoice item
+     */
     Invoice getInvoiceById(String apiKey, String invoiceId) throws StripeAPIException, ValidationException;
 }
